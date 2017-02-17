e9072bf8ef960278c583b13782a926e022b75ea4
Andreas Gruber <kariy2k9@gmail.com>
2017-02-17 13:43:51 +0100

    deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 8080;
 console.log("starting server on port " + port);
 
 var exec = require('child_process').exec;
-exec('./node_modules/.bin/webpack-dev-server --hot --inline --port ' + port, function (error, stdout, stderr) {
+exec('./node_modules/.bin/http-server -p ' + port, function (error, stdout, stderr) {
     sys.print('stdout: ' + stdout);
     sys.print('stderr: ' + stderr);
     if (error !== null) {
